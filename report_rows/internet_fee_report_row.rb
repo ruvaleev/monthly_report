1fd45b11861475acb39486dfda9f7208f233dc86
Ruslan Valeev <ruvaleev@gmail.com>
2020-07-20 11:54:10 +0700

    internet fee report row added

---
+++ report_rows/internet_fee_report_row.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'accumulative_account_based_report_row'
+
+class InternetFeeReportRow < AccumulativeAccountBasedReportRow
+end
---
+++ spec/report_rows/internet_fee_report_row_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+require_relative 'accumulative_account_based_shared_examples'
+require_relative '../../report_rows/internet_fee_report_row'
+
+RSpec.describe InternetFeeReportRow do
+  it_behaves_like 'accumulative_account_based_row', 'internet_fee'
+end
